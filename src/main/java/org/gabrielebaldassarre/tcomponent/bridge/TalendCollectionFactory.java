b33e72fd42d72317b22bc766f1b81e4d963c087f
Gabriele Baldassarre <info@9thcircle.it>
2012-12-13 01:21:01 +0100

    STUB

---
+++ src/main/java/org/gabrielebaldassarre/tcomponent/bridge/TalendCollectionFactory.java
@@ -0,0 +1,32 @@
+/*
+	This file is part of Talend Bridge Component API
+
+    Talend Bridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Talend Bridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.gabrielebaldassarre.tcomponent.bridge;
+
+import java.util.List;
+
+/**
+ * This is the factory used to build new collections to be used in the model. Since this factory has no sense outside
+ * of a model, use model's {@link getListFactory} to
+ * get an instance of the current factory.
+ * 
+ * @author Gabriele Baldassarre
+ *
+ */
+public interface TalendCollectionFactory {
+	
+	
+}
